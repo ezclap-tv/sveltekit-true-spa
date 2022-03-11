36c51f4b9f416c5ec7907e20bf3fc8c5076fc71c
jprochazk <1665677+jprochazk@users.noreply.github.com>
2022-03-11 18:11:05 +0100

    fix gh-pages base path

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 
+const basePath = (process.env.NODE_ENV === "production" && process.env.BASE_PATH) || "";
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
@@ -13,6 +15,9 @@ const config = {
       assets: "build",
       fallback: "index.html",
     }),
+    paths: {
+      base: basePath,
+    },
   },
 };
 
